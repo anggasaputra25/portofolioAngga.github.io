de207ee1a4731dab2709961ce718ba375c20e3fe
XIRPL2_I Ketut Angga Saputra_11 <anggasaputra2st@gmail.com>
2022-09-10 13:33:11 +0700

    Buat Ulang

--- script.js
+++ script.js
@@ -1,57 +1,66 @@
-const body = document.querySelector("body");
-const button = document.querySelector("button");
-const button2 = document.querySelector("#button");
-const menu = document.querySelector(".menu");
-const sidebar = document.querySelector("#sidebar");
-let loading = document.querySelector(".loading");
-var light = 0;
-var nilai = 0;
+let me = document.querySelector("#me");
+let team = document.querySelector("#team");
+let wordAngga = document.querySelector(".wordAngga");
+let wordTeam = document.querySelector(".wordTeam");
+let body = document.querySelector("body");
+let sun = document.querySelector("#sun")
+let kirim = document.querySelector("#kirim");
+let circle = document.querySelector("#circle");
+let nilai = 0;
+let nilai2 = 0;
 
-button.addEventListener("click", function(){
-    body.classList.toggle("light");
-    body.style.transition = "all  0.5s ease";
-    navbar.style.transition = "all  0.5s ease";
-    if(light == 0){
-        button.innerHTML = '<button><i class="fa-solid fa-moon"></i></button>';
-        button.style.margin = "0";
-        button.style.padding = "0";
-        light = 1;
-    }
-    else{ 
-        button.innerHTML = '<button><i class="fa-solid fa-sun"></i></button>';
-        light = 0;
-    }
-});
-
-button2.addEventListener("click", function(){
-    body.classList.toggle("light");
-    button2.innerHTML = '<button id="button">k</button>';
-    body.style.transition = "all  0.5s ease";
-    navbar.style.transition = "all  0.5s ease";
-    sidebar.style.transition = "all  0.5s ease";
-    if(light == 0){
-        button2.innerHTML = '<button><i class="fa-solid fa-moon"></i></button>';
-        light = 1;
-    }
-    else{ 
-        button2.innerHTML = '<button><i class="fa-solid fa-sun"></i></button>';
-        light = 0;
-    }
-});
-
-menu.addEventListener("click", function(){
+me.addEventListener("click", function(){
     if(nilai == 0){
-        sidebar.style.left = "0%";
-        sidebar.style.boxShadow = "20px 0px 20px rgba(0, 0, 0, 0.3)";
-        sidebar.style.transition = "all ease 1s";
+        wordAngga.style.animation = "forwards 3s wordAngga";
         nilai = 1;
     }
     else{
-        sidebar.style.left = "-50%";
-        sidebar.style.boxShadow = "0px 0px 0px";
+        wordAngga.style.animation = "forwards 3s wordAngga2";
         nilai = 0;
     }
 });
-window.addEventListener("load", function(){
-    loading.style.display = "none";
+team.addEventListener("click", function(){
+    if(nilai2 == 0){
+        wordTeam.style.animation = "forwards 3s wordTeam";
+        nilai2 = 1;
+    }
+    else{
+        wordTeam.style.animation = "forwards 3s wordTeam2";
+        nilai2 = 0;
+    }
+});
+sun.addEventListener("click", function(){
+    body.classList.toggle("light");
+    if(body.classList.contains("light")){
+        circle.style.animation = "forwards 2s animasiLight";
+    }
+    else{
+        circle.style.animation = "forwards 2s animasiLightRev";
+    }
+});
+kirim.addEventListener("click", function(){
+    alert("Pesan tidak dapat terkirim dikarenakan web ini belum mendukung fitur tersebut")
+});
+
+
+// menuBar
+let menu = document.querySelector(".menu");
+let line1 = document.querySelector(".line1");
+let line3 = document.querySelector(".line3");
+let sidebar = document.querySelector("#sidebar");
+let line = 0;
+
+menu.addEventListener("click", function(){
+    if(line == 0){
+        line1.style.animation = "forwards line1 1s";
+        line3.style.animation = "forwards line3 1s";
+        sidebar.style.left = "0";
+        line = 1;
+    }
+    else{
+        line1.style.animation = "forwards line1rev 1s";
+        line3.style.animation = "forwards line3rev 1s";
+        sidebar.style.left = "-60%";
+        line = 0;
+    }
 });
